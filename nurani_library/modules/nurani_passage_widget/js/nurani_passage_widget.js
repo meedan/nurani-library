ad1838ebab595d5f39b9169f70abf456f56967fa
James Andres <james.andres@gmail.com>
2012-12-11 12:50:40 -0500

    Adding tab-bar functionality for passage boxes

--- nurani_library/modules/nurani_passage_widget/js/nurani_passage_widget.js
+++ nurani_library/modules/nurani_passage_widget/js/nurani_passage_widget.js
@@ -29,13 +29,56 @@ var PassageWidget = (function ($) {
     // Note the page anchor fragment marker '#' is abused to also be the
     // ID marker for jQuery
     matches = widget.original_url.match(/#passage-widget-[a-z0-9]{8,32}$/);
-    if (matches[0]) {
-      this.addWidget(matches[0], widget.html)
+
+    this.$element = matches[0] ? $(matches[0]) : false;
+
+    if (this.$element) {
+      this.addWidget(widget.html)
+      this.addWidgetTabBar();
     }
   };
 
-  PassageWidget.prototype.addWidget = function(selector, html) {
-    $(selector).html(html);
+  PassageWidget.prototype.addWidget = function(html) {
+    this.$element.html(html);
+  };
+
+  PassageWidget.prototype.addWidgetTabBar = function() {
+    var that = this,
+        $passageWidgets = this.$element.find('> .passage-widget'),
+        first = true;
+
+    if ($passageWidgets.length <= 1) {
+      return;
+    }
+
+    this.$tabBar = $('<div class="tab-bar"></div>');
+
+    this.$element
+      .parent()
+      .append(this.$tabBar);
+
+    $passageWidgets.each(function () {
+      var $this = $(this),
+          title = $this.find('h4').text(),
+          $tab  = $('<a href="#" class="button">' + title + '</a>');
+
+      if (!first) {
+        $this.hide();
+      }
+
+      $tab.data('passage-widget', $this);
+      $tab.click(function () { that.tabSwitchAction(this); });
+
+      that.$tabBar.append($tab);
+
+      first = false;
+    });
+
+  };
+
+  PassageWidget.prototype.tabSwitchAction = function (tab) {
+    $('.passage-widget', this.$element).hide();
+    $(tab).data('passage-widget').show();
   };
 
   return PassageWidget;
